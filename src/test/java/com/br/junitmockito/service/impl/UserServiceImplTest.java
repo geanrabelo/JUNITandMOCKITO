e11de5665837a8768c1d09cb72ece66acf22cfe8
geanrabelo <geancardosorabelomota@gmail.com>
2025-04-28 19:10:11 -0300

    Testes para deleteById realizados com os dois cenários possíveis

--- src/test/java/com/br/junitmockito/service/impl/UserServiceImplTest.java
+++ src/test/java/com/br/junitmockito/service/impl/UserServiceImplTest.java
@@ -1,8 +1,10 @@
 package com.br.junitmockito.service.impl;
 
 import com.br.junitmockito.domain.User;
+import com.br.junitmockito.dto.UserCreateDTO;
 import com.br.junitmockito.dto.UserDetailsDTO;
 import com.br.junitmockito.dto.UserUpdateDTO;
+import com.br.junitmockito.exceptions.ex.UserEmailAlreadyExists;
 import com.br.junitmockito.exceptions.ex.UserNotFound;
 import com.br.junitmockito.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -12,13 +14,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -31,6 +30,8 @@ class UserServiceImplTest {
 
     private UserDetailsDTO userDetailsDTO;
 
+    private UserCreateDTO userCreateDTO;
+
     private User user;
 
     private UserUpdateDTO userUpdateDTO;
@@ -41,7 +42,8 @@ class UserServiceImplTest {
     private static final String NAME = "Gean";
     private static final String EMAIL = "gean@gmail.com";
     private static final String PASSWORD = "123";
-    private static final String MESSAGEUSERNOTFOUND = "User find by id not found";
+    private static final String MESSAGE_USER_NOT_FOUND = "User find by id not found";
+    private static final String MESSAGE_USER_ALREADY_EXISTS_EMAIL = "Email of user already exists in database. Try again";
 
     @BeforeEach
     void setUp() {
@@ -49,11 +51,62 @@ class UserServiceImplTest {
     }
 
     @Test
-    void save() {
+    void whenSaveThenReturnSuccess() {
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+
+        Mockito.when(userRepository.existsByEmail(Mockito.any())).thenReturn(false);
+
+        User response = userServiceImpl.save(userCreateDTO);
+
+        Assertions.assertEquals(User.class, response.getClass());
     }
 
     @Test
-    void update() {
+    void whenSaveThenReturnUserEmailAlreadyExists(){
+        Mockito.when(userRepository
+                .existsByEmail(Mockito.any()))
+                .thenThrow(new UserEmailAlreadyExists(MESSAGE_USER_ALREADY_EXISTS_EMAIL));
+
+        try{
+
+            userServiceImpl.save(userCreateDTO);
+
+        }catch (Exception ex){
+            Assertions.assertEquals(UserEmailAlreadyExists.class, ex.getClass());
+            Assertions.assertEquals(MESSAGE_USER_ALREADY_EXISTS_EMAIL, ex.getMessage());
+        }
+    }
+
+    @Test
+    void whenUpdateThenReturnSuccess() {
+        Mockito.when(userRepository.existsById(Mockito.any())).thenReturn(true);
+        Mockito.when(userRepository.existsByEmail(Mockito.any())).thenReturn(false);
+        Mockito.when(userRepository.getReferenceById(Mockito.any())).thenReturn(user);
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+
+        User response = userServiceImpl.update(userUpdateDTO);
+
+        Assertions.assertEquals(User.class, response.getClass());
+        Assertions.assertEquals(EMAIL, response.getEmail());
+        Assertions.assertEquals(NAME, response.getName());
+        Assertions.assertEquals(PASSWORD, response.getPassword());
+        Assertions.assertEquals(ID, response.getId());
+    }
+
+    @Test
+    void whenUpdateThenReturnFailed(){
+        Mockito.when(userRepository.existsById(Mockito.any())).thenThrow(new UserNotFound(MESSAGE_USER_NOT_FOUND));
+
+        try{
+
+            userServiceImpl.update(userUpdateDTO);
+
+        }catch (Exception ex){
+
+            Assertions.assertEquals(UserNotFound.class, ex.getClass());
+            Assertions.assertEquals(MESSAGE_USER_NOT_FOUND, ex.getMessage());
+
+        }
     }
 
     @Test
@@ -70,27 +123,43 @@ class UserServiceImplTest {
 
     @Test
     void whenFindByIdThenReturnUserNotFoundHandler(){
-        Mockito.when(userRepository.findById(ID)).thenThrow(new UserNotFound(MESSAGEUSERNOTFOUND));
+        Mockito.when(userRepository.findById(ID)).thenThrow(new UserNotFound(MESSAGE_USER_NOT_FOUND));
 
         try{
             userServiceImpl.findById(ID);
         }catch (Exception ex){
             Assertions.assertEquals(UserNotFound.class, ex.getClass());
-            Assertions.assertEquals(MESSAGEUSERNOTFOUND, ex.getMessage());
+            Assertions.assertEquals(MESSAGE_USER_NOT_FOUND, ex.getMessage());
         }
     }
 
     @Test
-    void deleteById() {
+    void deleteWithSuccess() {
+        Mockito.when(userRepository.existsById(Mockito.any())).thenReturn(true);
+        Mockito.doNothing().when(userRepository).deleteById(Mockito.any());
+
+        userServiceImpl.deleteById(ID);
     }
 
     @Test
-    void findAll() {
+    void deleteWithoutSuccess(){
+        Mockito.when(userRepository.existsById(Mockito.any())).thenThrow(new UserNotFound(MESSAGE_USER_NOT_FOUND));
+
+        try {
+            userServiceImpl.deleteById(ID);
+        }catch (Exception ex){
+            Assertions.assertEquals(UserNotFound.class, ex.getClass());
+        }
+    }
+
+    @Test
+    void whenFindAllThenReturnListOfUserDetailsDTO() {
         Mockito.when(userRepository.findAll()).thenReturn(List.of(user));
 
         List<UserDetailsDTO> response = userServiceImpl.findAll();
 
         Assertions.assertEquals(userDetailsDTOList, response);
+        Assertions.assertEquals(UserDetailsDTO.class, response.get(0).getClass());
     }
 
     @Test
@@ -106,5 +175,6 @@ class UserServiceImplTest {
         userDetailsDTO = new UserDetailsDTO(ID, NAME, EMAIL);
         userUpdateDTO = new UserUpdateDTO(ID, EMAIL);
         userDetailsDTOList = List.of(new UserDetailsDTO(user));
+        userCreateDTO = new UserCreateDTO(NAME, EMAIL, PASSWORD);
     }
 }
